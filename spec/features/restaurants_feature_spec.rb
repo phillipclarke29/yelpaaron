e52fd0ecde47da5e597cd040c8dacf8d386f0155
Aaron <aarongkendall@gmail.com>
2015-10-13 21:31:11 +0100

    User must be logged in to make changes

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -30,6 +30,11 @@ feature 'restaurants' do
 
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
       visit '/restaurants'
+      click_link 'Sign up'
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
+      click_button 'Sign up'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
       click_button 'Create Restaurant'
@@ -37,10 +42,21 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants'
     end
 
+    scenario 'guest cannot add a restaurant' do
+       visit '/'
+       click_link 'Add a restaurant'
+       expect(current_path).to eq '/users/sign_in'
+    end
+
     context 'an invalid restaurant' do
 
       it 'does not let you submit a name that is too short' do
         visit '/restaurants'
+        click_link 'Sign up'
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Password', with: 'testtest'
+        fill_in 'Password confirmation', with: 'testtest'
+        click_button 'Sign up'
         click_link 'Add a restaurant'
         fill_in 'Name', with: 'kf'
         click_button 'Create Restaurant'
@@ -63,6 +79,11 @@ feature 'restaurants' do
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
 
+    scenario 'guest can see restaurants' do
+      visit '/'
+      expect(page).to have_content 'KFC'
+    end
+
   end
 
   context 'editing restaurants' do
@@ -71,6 +92,11 @@ feature 'restaurants' do
 
     scenario 'let a user edit a restaurant' do
       visit '/restaurants'
+      click_link 'Sign up'
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
+      click_button 'Sign up'
       click_link 'Edit KFC'
       fill_in 'Name', with: 'Kentucky Fried Chicken'
       click_button 'Update Restaurant'
@@ -86,6 +112,11 @@ feature 'restaurants' do
 
     scenario 'removes a restaurant when the user clicks a delete link' do
       visit '/restaurants'
+      click_link 'Sign up'
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
+      click_button 'Sign up'
       click_link 'Delete KFC'
       expect(page).not_to have_content 'KFC'
       expect(page).to have_content 'Restaurant deleted successfully'
