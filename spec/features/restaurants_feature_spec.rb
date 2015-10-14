235c652caf1e32bb079e6cc9c5b50b9e2ee94796
Aaron <aarongkendall@gmail.com>
2015-10-14 13:47:20 +0100

    refactoring with factory girl

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -52,11 +52,8 @@ feature 'restaurants' do
 
       it 'does not let you submit a name that is too short' do
         visit '/restaurants'
-        click_link 'Sign up'
-        fill_in 'Email', with: 'test@test.com'
-        fill_in 'Password', with: 'testtest'
-        fill_in 'Password confirmation', with: 'testtest'
-        click_button 'Sign up'
+        user = build(:user)
+        sign_up(user.email, user.password)
         click_link 'Add a restaurant'
         fill_in 'Name', with: 'kf'
         click_button 'Create Restaurant'
@@ -92,11 +89,8 @@ feature 'restaurants' do
 
     scenario 'let a user edit a restaurant' do
       visit '/restaurants'
-      click_link 'Sign up'
-      fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'testtest'
-      fill_in 'Password confirmation', with: 'testtest'
-      click_button 'Sign up'
+      user = build(:user)
+      sign_up(user.email, user.password)
       click_link 'Edit KFC'
       fill_in 'Name', with: 'Kentucky Fried Chicken'
       click_button 'Update Restaurant'
@@ -112,11 +106,8 @@ feature 'restaurants' do
 
     scenario 'removes a restaurant when the user clicks a delete link' do
       visit '/restaurants'
-      click_link 'Sign up'
-      fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'testtest'
-      fill_in 'Password confirmation', with: 'testtest'
-      click_button 'Sign up'
+      user = build(:user)
+      sign_up(user.email, user.password)
       click_link 'Delete KFC'
       expect(page).not_to have_content 'KFC'
       expect(page).to have_content 'Restaurant deleted successfully'
